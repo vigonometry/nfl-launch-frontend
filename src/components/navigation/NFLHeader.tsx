1e480c4f69a0acedbdf254b5a45c6865bc5ba5c6
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2022-07-13 11:19:40 +0800

    Update NFLHeader.tsx

--- src/components/navigation/NFLHeader.tsx
+++ src/components/navigation/NFLHeader.tsx
@@ -1,14 +1,23 @@
-import { Burger, Group, Header, Image, MediaQuery } from "@mantine/core"
+import { Burger, createStyles, Group, Header, Image, MediaQuery } from "@mantine/core"
 import SiteNavigationButtons from "./SiteNavigationButtons"
 
 const logo = require('./assets/nfl-logo.png')
 
+const useStyles = createStyles((theme) => ({
+    img: {
+        [theme.fn.smallerThan('sm')]: {
+            display: "none"
+        }
+    }
+}))
+
 const NFLHeader = ({opened, toggle}: {opened: boolean, toggle: () => void}) => {
+    const { classes } = useStyles()
 
     return (
         <Header height={84} p='xl'>
             <Group position="apart">
-                <Image src={logo} height={40}/>
+                <Image src={logo} height={50} className={classes.img}/>
                 <MediaQuery smallerThan='sm' styles={{display: 'none'}}>
                     <Group>
                         <SiteNavigationButtons smallScreen={false}/>
